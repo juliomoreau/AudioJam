cbdb89b87ddc0315e42228f281f90f36c3c1fb48
Joseph Sallembien <josallembien@gmail.com>
2017-01-23 18:05:33 +0100

    facebook integration part 1 (missing key hashes etc.)

--- app/src/main/java/com/example/jules/audiojam/LoginActivity.java
+++ app/src/main/java/com/example/jules/audiojam/LoginActivity.java
@@ -32,6 +32,7 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.facebook.FacebookSdk;
 import static android.Manifest.permission.READ_CONTACTS;
 
 /**
