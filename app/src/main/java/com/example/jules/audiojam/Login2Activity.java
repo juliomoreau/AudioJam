f687e36a42dd1a3c9fd274a8b19ccce05ade01ac
juliomoreau <julesmoreau76@gmail.com>
2017-01-23 14:27:08 +0100

    Begining login activity

---
+++ app/src/main/java/com/example/jules/audiojam/Login2Activity.java
@@ -0,0 +1,15 @@
+package com.example.jules.audiojam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
