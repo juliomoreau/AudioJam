2b5549ab0c71e81596e00feb1ac3874b1b3e89d0
juliomoreau <julesmoreau76@gmail.com>
2017-01-23 15:28:31 +0100

    Login activity tuto

---
+++ app/src/main/java/com/example/jules/audiojam/AppController.java
@@ -0,0 +1,15 @@
+package com.example.jules.audiojam;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+/**
+ * Created by Jules on 23/01/2017.
+ */
+
+public class AppController {
+
+}
