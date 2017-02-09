0ca8bae27ffac7bb9481640d84ea3baa7280be26
Joseph Sallembien <josallembien@gmail.com>
2017-02-09 16:37:34 +0100

    t

--- app/src/main/java/com/example/jules/audiojam/Main2Activity.java
+++ app/src/main/java/com/example/jules/audiojam/Main2Activity.java
@@ -142,7 +142,7 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
