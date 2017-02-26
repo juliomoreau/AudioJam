eac985e2c06b046f832188f2e5e923474635574f
juliomoreau <julesmoreau76@gmail.com>
2017-02-26 19:51:37 +0100

    Added yt player

--- app/src/main/java/com/example/jules/audiojam/LiveFragment.java
+++ app/src/main/java/com/example/jules/audiojam/LiveFragment.java
@@ -3,9 +3,16 @@ package com.example.jules.audiojam;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 /**
  * Created by domicile on 24/02/2017.
@@ -13,9 +20,42 @@ import android.view.ViewGroup;
 
 public class LiveFragment extends Fragment {
 
+    private static final String API_KEY = "AIzaSyBvPumEkyjBtjjEKAWECHbEEk7RTGo3ZNU";
+    private static String VIDEO_ID = "a4NT5iBFuZs";
+
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.livelayout, null);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.livelayout, container, false);
+
+        YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
+
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit();
+
+        //Initialise le player
+        youTubePlayerFragment.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player, boolean wasRestored) {
+                if (!wasRestored) {
+                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+                    player.loadVideo(VIDEO_ID);
+                }
+            }
+
+            // YouTubeプレーヤーの初期化失敗
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult error) {
+                // YouTube error
+                String errorMessage = error.toString();
+                Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+                Log.d("errorMessage:", errorMessage);
+            }
+        });
+        return rootView;
+
     }
 }
