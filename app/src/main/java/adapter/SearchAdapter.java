9c908898a6c2da2d0825147d1631a6f9c9833445
juliomoreau <julesmoreau76@gmail.com>
2017-04-17 15:36:51 +0200

    Youtube Search working fine

---
+++ app/src/main/java/adapter/SearchAdapter.java
@@ -0,0 +1,70 @@
+package adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jules.audiojam.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import entities.ResultSearch;
+
+
+/**
+ * Created by Jules on 16/04/2017.
+ */
+
+public class SearchAdapter extends BaseAdapter {
+    Context context;
+    Activity activity;
+
+
+    private  static  LayoutInflater inflater;
+    private List<ResultSearch> items;
+
+    public SearchAdapter(Context c, Activity a, List<ResultSearch> searchList){
+        context=c;
+        activity=a;
+        items=searchList;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addResultSearch(ResultSearch item) {
+        items.add(item);
+    }
+
+    public int getCount() {
+        return items.size();
+    }
+
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    public long getItemId(int position){
+        return position;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        View vi = convertView;
+        vi =   inflater.inflate(R.layout.row_layout,null);
+        ImageView image = (ImageView) vi.findViewById(R.id.list_image);
+        TextView nom = (TextView) vi.findViewById(R.id.title);
+
+        ResultSearch resultSearch = items.get(position);
+        Picasso.with(context).load(resultSearch.getThumbnail().getUrl()).into(image);
+        nom.setText(resultSearch.getVideoName());
+
+
+
+        return vi;
+    }
+
+}
--- app/src/main/java/com/example/jules/audiojam/MusicFragment.java
+++ app/src/main/java/com/example/jules/audiojam/MusicFragment.java
@@ -1,11 +1,40 @@
 package com.example.jules.audiojam;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+import java.io.IOException;
+import java.nio.BufferUnderflowException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import adapter.SearchAdapter;
+import entities.ResultSearch;
+import helper.Auth;
+
 
 /**
  * Created by domicile on 24/02/2017.
@@ -13,9 +42,144 @@ import android.view.ViewGroup;
 
 public class MusicFragment extends Fragment {
 
+    /**
+     * Define a global variable that identifies the name of a file that
+     * contains the developer's API key.
+     */
+
+    private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+    /**
+     * Define a global instance of a Youtube object, which will be used
+     * to make YouTube Data API requests.
+     */
+    private static YouTube youtube;
+
+
+    Button button;
+    EditText editText;
+    SearchAdapter adapter;
+    ListView lv;
+    String search;
+
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.musiclayout, null);
+
+
+
     }
+
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        button = (Button) getView().findViewById(R.id.button4);
+        editText = (EditText) getView().findViewById(R.id.editText2);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    // This object is used to make YouTube Data API requests. The last
+                    // argument is required, but since we don't need anything
+                    // initialized when the HttpRequest is initialized, we override
+                    // the interface and provide a no-op function.
+                    youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+                        public void initialize(HttpRequest request) throws IOException {
+                        }
+                    }).setApplicationName("youtube-cmdline-search-sample").build();
+
+
+                    // Gets the term searched and convert it to a string.
+                    String queryTerm = search = editText.getText().toString();
+
+
+                    // Define the API request for retrieving search results.
+                    YouTube.Search.List search = youtube.search().list("id,snippet");
+
+                    // Set your developer key from the {{ Google Cloud Console }} for
+                    // non-authenticated requests. See:
+                    // {{ https://cloud.google.com/console }}
+                    String apiKey = "AIzaSyAnhCNVBVq-j3sBkhBZUDW8TVlXIjSX648";
+                    search.setKey(apiKey);
+                    search.setQ(queryTerm);
+
+                    // Restrict the search results to only include videos. See:
+                    // https://developers.google.com/youtube/v3/docs/search/list#type
+                    search.setType("video");
+
+                    // To increase efficiency, only retrieve the fields that the
+                    // application uses.
+                    search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+                    search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+
+                    // Call the API and print results.
+                    SearchListResponse searchResponse = search.execute();
+                    List<SearchResult> searchResultList = searchResponse.getItems();
+                    if (searchResultList != null) {
+                        prettyPrint(searchResultList.iterator(), queryTerm);
+
+                    }
+                } catch (GoogleJsonResponseException e) {
+                    System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                            + e.getDetails().getMessage());
+                } catch (IOException e) {
+                    System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+
+    /*
+   * Prints out all results in the Iterator. For each result, print the
+   * title, video ID, and thumbnail.
+   *
+   * @param iteratorSearchResults Iterator of SearchResults to print
+   *
+   * @param query Search query (String)
+   */
+    private void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+        List<ResultSearch> list = new ArrayList<ResultSearch>();
+
+        if (!iteratorSearchResults.hasNext()) {
+            System.out.println(" There aren't any results for your query.");
+        }
+
+        while (iteratorSearchResults.hasNext()) {
+
+            SearchResult singleVideo = iteratorSearchResults.next();
+            ResourceId rId = singleVideo.getId();
+
+            // Confirm that the result represents a video. Otherwise, the
+            // item will not contain a video ID.
+            if (rId.getKind().equals("youtube#video")) {
+                Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
+                ResultSearch resultat = new ResultSearch(singleVideo.getSnippet().getTitle(),thumbnail.getUrl(),rId.getVideoId(),thumbnail);
+
+                list.add(resultat);
+
+
+            }
+
+        }
+        lv=(ListView) getView().findViewById(R.id.listView);
+
+        adapter = new SearchAdapter(getActivity(),getActivity(),list);
+        lv.setAdapter(adapter);
+    }
+
+
+
 }
