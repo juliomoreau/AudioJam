a718560dd1042e6b29115b8bbcb6bd55146b3e81
juliomoreau <julesmoreau76@gmail.com>
2017-04-25 16:23:37 +0200

    Youtube Search nice searchbar

--- app/src/main/java/com/example/jules/audiojam/MusicFragment.java
+++ app/src/main/java/com/example/jules/audiojam/MusicFragment.java
@@ -4,7 +4,14 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -12,8 +19,10 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import android.support.v7.widget.Toolbar;
+import android.app.ActionBar;
 import android.widget.SearchView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
@@ -24,9 +33,10 @@ import com.google.api.services.youtube.model.ResourceId;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Thumbnail;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 import java.io.IOException;
-import java.nio.BufferUnderflowException;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -36,11 +46,13 @@ import entities.ResultSearch;
 import helper.Auth;
 
 
+
+
 /**
  * Created by domicile on 24/02/2017.
  */
 
-public class MusicFragment extends Fragment {
+public class MusicFragment extends Fragment implements MaterialSearchBar.OnSearchActionListener {
 
     /**
      * Define a global variable that identifies the name of a file that
@@ -61,6 +73,10 @@ public class MusicFragment extends Fragment {
     SearchAdapter adapter;
     ListView lv;
     String search;
+    MaterialSearchBar searchBar;
+
+
+
 
 
     @Nullable
@@ -73,73 +89,87 @@ public class MusicFragment extends Fragment {
 
     }
 
+
+
+
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        button = (Button) getView().findViewById(R.id.button4);
-        editText = (EditText) getView().findViewById(R.id.editText2);
+        searchBar = (MaterialSearchBar) getView().findViewById(R.id.sB);
+        searchBar.setHint("Recherche ...");
+        searchBar.setOnSearchActionListener(this);
 
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                try {
-                    // This object is used to make YouTube Data API requests. The last
-                    // argument is required, but since we don't need anything
-                    // initialized when the HttpRequest is initialized, we override
-                    // the interface and provide a no-op function.
-                    youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
-                        public void initialize(HttpRequest request) throws IOException {
-                        }
-                    }).setApplicationName("youtube-cmdline-search-sample").build();
-
-
-                    // Gets the term searched and convert it to a string.
-                    String queryTerm = search = editText.getText().toString();
-
-
-                    // Define the API request for retrieving search results.
-                    YouTube.Search.List search = youtube.search().list("id,snippet");
-
-                    // Set your developer key from the {{ Google Cloud Console }} for
-                    // non-authenticated requests. See:
-                    // {{ https://cloud.google.com/console }}
-                    String apiKey = "AIzaSyAnhCNVBVq-j3sBkhBZUDW8TVlXIjSX648";
-                    search.setKey(apiKey);
-                    search.setQ(queryTerm);
-
-                    // Restrict the search results to only include videos. See:
-                    // https://developers.google.com/youtube/v3/docs/search/list#type
-                    search.setType("video");
-
-                    // To increase efficiency, only retrieve the fields that the
-                    // application uses.
-                    search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
-                    search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
-
-                    // Call the API and print results.
-                    SearchListResponse searchResponse = search.execute();
-                    List<SearchResult> searchResultList = searchResponse.getItems();
-                    if (searchResultList != null) {
-                        prettyPrint(searchResultList.iterator(), queryTerm);
-
-                    }
-                } catch (GoogleJsonResponseException e) {
-                    System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
-                            + e.getDetails().getMessage());
-                } catch (IOException e) {
-                    System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
-                } catch (Throwable t) {
-                    t.printStackTrace();
-                }
 
             }
-        });
+
+    @Override
+    public void onSearchStateChanged(boolean enabled) {
+
     }
+
+    @Override
+    public void onSearchConfirmed(CharSequence text) {
+        try {
+            // This object is used to make YouTube Data API requests. The last
+            // argument is required, but since we don't need anything
+            // initialized when the HttpRequest is initialized, we override
+            // the interface and provide a no-op function.
+            youtube = new YouTube.Builder(Auth.HTTP_TRANSPORT, Auth.JSON_FACTORY, new HttpRequestInitializer() {
+                public void initialize(HttpRequest request) throws IOException {
+                }
+            }).setApplicationName("youtube-cmdline-search-sample").build();
+
+
+            // Gets the term searched and convert it to a string.
+            String queryTerm = search = text.toString();
+
+
+            // Define the API request for retrieving search results.
+            YouTube.Search.List search = youtube.search().list("id,snippet");
+
+            // Set your developer key from the {{ Google Cloud Console }} for
+            // non-authenticated requests. See:
+            // {{ https://cloud.google.com/console }}
+            String apiKey = "AIzaSyAnhCNVBVq-j3sBkhBZUDW8TVlXIjSX648";
+            search.setKey(apiKey);
+            search.setQ(queryTerm);
+
+            // Restrict the search results to only include videos. See:
+            // https://developers.google.com/youtube/v3/docs/search/list#type
+            search.setType("video");
+
+            // To increase efficiency, only retrieve the fields that the
+            // application uses.
+            search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+            search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+
+            // Call the API and print results.
+            SearchListResponse searchResponse = search.execute();
+            List<SearchResult> searchResultList = searchResponse.getItems();
+            if (searchResultList != null) {
+                prettyPrint(searchResultList.iterator(), queryTerm);
+
+            }
+        } catch (GoogleJsonResponseException e) {
+            System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                    + e.getDetails().getMessage());
+        } catch (IOException e) {
+            System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    public void onButtonClicked(int buttonCode) {
+
+        }
+
 
 
     /*
@@ -179,6 +209,9 @@ public class MusicFragment extends Fragment {
         adapter = new SearchAdapter(getActivity(),getActivity(),list);
         lv.setAdapter(adapter);
     }
+
+
+
 
 
 
